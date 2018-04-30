3b451443a2a12db58a4cf7c585052e51c025651d
axp <645124937@qq.com>
2018-04-30 12:17:41 +0800

    fix allocate时直接会初始化对象

--- construct.h
+++ construct.h
@@ -6,12 +6,20 @@
 #ifndef _CUSTL_CONSTRUCT_H
 #define _CUSTL_CONSTRUCT_H
 
+#include <new>
+
 #include "iterator.h"
 
 namespace custl {
+// By axp:
+// placement new依赖于全局的void* operator new(sizetype, void*);
+// 且此函数不能定义于namespace中，如果手写此函数会导致之后包含stl标准库时
+// 因重复定义此函数而报错，故此处引入了stl的new文件。
+// 如果之后custl足够完善，可保证不引入标准库文件的情况下也不影响库的开发和
+// 使用，可在全局补充该函数。
 template <typename T1, typename T2>
 inline void construct(T1* p, const T2& value) {
-    new (p) T1(value);
+    new(p) T1(value);
 }
 template <typename T>
 inline void destory(T* p) {
